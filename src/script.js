fcc1382994aba4c83ef6f26871b8304117c7405e
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2023-01-25 15:47:38 +0400

    Add ethers.js lib for decoding. Almost working app

--- src/script.js
+++ src/script.js
@@ -1,13 +1,43 @@
+//Crypto setup
+import { ethers } from 'ethers';
+import { Network, Alchemy } from 'alchemy-sdk';
+const { API_KEY } = process.env;
+const settings = {
+  apiKey: API_KEY,
+  network: Network.ETH_MAINNET,
+};
+const alchemy = new Alchemy(settings);
+
 // адреса для примера
 const ethAddressMockup = [
-  '0xD0d03f101A6E31a7fc1F1b843BA0b6FD33250991',
-  '0x4633084D5815637701758871610c12bFC2A384fF',
-  '0x4b6f9B6920D6c919ce59c3e89d07D579490b91b3',
-  '0x59314B46124ECDDe2ADD15D9f89e37096a6D66Ef',
-  '0x3528CE634eC90aE57BAB14A3A2bcb69E162214d4',
-  '0x67F56E93d318F1D7C59A37069f01D5ed7790Ef15',
+  '0xA626B50233F1157CE74060a8affc0855c1868466',
+  '0xc2f6f3a04dfd0107afeb31219d423acab2a32821',
+  '0x1b165fE4DA6bC58AB8370DDC763d367D29F50Ef0',
+  '0x033edE126bc089e6a63226c669E1AD02fa5890E2',
+  '0x1E136032A34092EeAFB7855061110F8433e379BA',
 ];
 const ethAddressMockupList = document.querySelector('.mockup-list');
 ethAddressMockup.map((el) => {
   ethAddressMockupList.innerHTML += `<li>${el}</li>`;
+});
+
+//проверка баланса
+const buttonBalance = document.querySelector('#test-cash');
+const ethAddress = document.querySelector('#eth-address');
+const cashOutput = document.querySelector('#cash-output');
+
+buttonBalance.addEventListener('click', () => {
+  let eth = ethAddress.value;
+  if (eth) {
+    //ищем баланс и выводим
+    alchemy.core
+      .getBalance(eth, 'latest')
+      .then((response) => balanceOutput(response));
+  }
+
+  function balanceOutput(res) {
+    // c hex-числами ETH нельзя работать напрямую, потому что они за пределами возможностей языка Javascript, поэтому используется библиотека Ethers.js
+    const cash = ethers.utils.formatEther(parseInt(res._hex).toString());
+    cashOutput.innerHTML = `Баланс: ${cash} ETH.`;
+  }
 });
